056d8bf9c455a57db7e70fd88bfbfe34bdc6b1c9
sanders yao <sanders.yao@gmail.com>
2016-02-02 15:52:11 +0800

    变更权限 开始使用命名空间whisper‘

--- init.inc.php
+++ init.inc.php
@@ -4,17 +4,17 @@
  *
  * @author  yaoxiaowei
  */
+namespace   whisper;
 require_once    dirname(__FILE__) . '/config/config.inc.php';
 require_once    LIB . 'Application.class.php';
-require_once    'MI.php';
 
 date_default_timezone_set(TIME_ZONE_DEFAULT);
 
 Application::initialize();
 
 //xhprof initailize
-$xhprofPath = isset($_SERVER) && isset($_SERVER['PHP_SELF'])    ? $_SERVER['PHP_SELF']  : $argv[0];
-$xhprofMark = str_replace(array('.', '/'), '_', $xhprofPath);
-XHProfAnalysis::initialize($xhprofMark);
-register_shutdown_function('XHProfAnalysis::save');
+//$xhprofPath = isset($_SERVER) && isset($_SERVER['PHP_SELF'])    ? $_SERVER['PHP_SELF']  : $argv[0];
+//$xhprofMark = str_replace(array('.', '/'), '_', $xhprofPath);
+//XHProfAnalysis::initialize($xhprofMark);
+//register_shutdown_function('XHProfAnalysis::save');
 
